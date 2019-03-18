82613fc039174ba93f1ba946904156b8d6237df2
Emil Kjelsrud <emilkje@gmail.com>
2019-03-18 17:36:59 +0100

    Moved variables from linked template into master

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 LOC='westeurope'
 GROUP='DevOpsDemo'
-TEMPL='MyFirstArmDeployment.json'
-PARAMS_FILE='MyFirstArmDeployment.parameters.json'
+TEMPL='azuredeploy.json'
+PARAMS_FILE='azuredeploy.parameters.json'
 
 az group deployment create -g $GROUP --template-file $TEMPL --parameters $PARAMS_FILE
